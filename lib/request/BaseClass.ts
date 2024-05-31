2f5bdb5ab599fa02881dcfd6d6bd822add7ba1dc
CI Bot Apollopayment <ci_bot@apollopayment.io>
2024-05-31 17:55:59 +0000

    updates and improvements

--- lib/request/BaseClass.ts
+++ lib/request/BaseClass.ts
@@ -1,33 +1,5 @@
-import { Axios } from 'axios';
-
-import { CustomHeaderBuilder } from '../DataBuilder';
-
-import { TNullableString } from '../types/Base';
-
-export type ApolloAPIResponse = {
-  errors?: TNullableString,
-  error?: TNullableString,
-  //[key: string]: string | number | boolean | object
-}
-
-export type ApolloAPIReturnType = Promise<ApolloAPIResponse | string | boolean>;
+import {ApolloPayment} from "../index";
 
 export default class BaseClass {
-  protected headerBuilder: CustomHeaderBuilder;
-  protected axiosInstance: Axios;
-
-  constructor(axiosInstance: Axios, headerBuilder: CustomHeaderBuilder) {
-    this.headerBuilder = headerBuilder;
-    this.axiosInstance = axiosInstance;
-  }
-
-  // @ts-ignore
-  protected async exceptionWrapper<T extends Function>(cb: T): ReturnType<T> {
-    try {
-      return await cb();
-    } catch (exp: any) {
-      //console.log('Error occured', exp.message);
-      throw new Error(exp.message);
-    }
-  }
+  constructor(protected core: ApolloPayment) {}
 }
